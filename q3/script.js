9287336b0de5b72926c755ba6aaefe869a7e22cd
Thomas Shaddock <tomshaddock@googlemail.com>
2017-04-04 19:01:00 +0100

    git add missing file

--- q3/script.js
+++ q3/script.js
@@ -3,6 +3,8 @@
 	Please note that I have installed the additional-modules extension for the plugin as it allows me to do regex pattern matching
 	inside the individual html input elements. This kept things more simple and saved me a whole load of time as I didnt have to craft
 	custom rules or custom functions to achieve this.
+	Also please note that I have used CDNs to load the 2 external scripts and thus when marking this peice of work it must be done on a pc
+	that can access the internet.
 */
 
 function init(){
